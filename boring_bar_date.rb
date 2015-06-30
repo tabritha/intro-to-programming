b5dd32a3ced4a25aa84f2aade575e09b6f0defbb
Ariel <tabritha@Macs-MacBook-Pro.local>
2015-06-29 21:52:58 -0400

    HW week 3 finished

---
+++ boring_bar_date.rb
@@ -0,0 +1,11 @@
+
+chat = gets.chomp
+year = prng.rand(1984..1998)
+
+while true
+
+if chat == chat.upcase
+  puts "NO, NOT SINCE" year "!"
+
+else
+  puts "Hu!?!? Um - YA! I CAN\'T HEAR YOU! THE MUSIC\'S TOO LOUD!"
--- bottles_of_beer.rb
+++ bottles_of_beer.rb
@@ -8,7 +8,8 @@ if bottles == 1
 	break
 end
 
-	puts bottles.to_s + " bottles of beer on the wall, " + bottles.to_s + " bottles of beer.
-	Take one down and pass it around, " + (bottles - 1).to_s + " bottles of beer on the wall."
+	puts bottles.to_s + " bottles of beer on the wall, "
+	+ bottles.to_s + " bottles of beer. Take one down and pass it around, "
+	+ (bottles - 1).to_s + " bottles of beer on the wall."
 
 end
