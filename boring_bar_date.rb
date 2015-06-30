5a01162b09096d5008fd41e9b9afb97bcce5e835
Ariel <tabritha@Macs-MacBook-Pro.local>
2015-06-29 22:28:49 -0400

    Homework tested

--- boring_bar_date.rb
+++ boring_bar_date.rb
@@ -1,11 +1,15 @@
-
-chat = gets.chomp
-year = prng.rand(1984..1998)
-
 while true
 
-if chat == chat.upcase
-  puts "NO, NOT SINCE" year "!"
+chat = gets.chomp
+year = rand(1984..1998).to_s
+
+if chat == 'BYE'
+        break
+
+elsif chat == chat.upcase
+  puts "NO, NOT SINCE " + year + "!"
 
 else
-  puts "Hu!?!? Um - YA! I CAN\'T HEAR YOU! THE MUSIC\'S TOO LOUD!"
+  puts "Hu!?!? Um - YA! I CAN'T HEAR YOU! THE MUSIC'S TOO LOUD!"
+end
+end
